1153062355acb980a9f544b181dd6ee7f092aba1
Fabian Deutsch <fabiand@fedoraproject.org>
2015-02-17 12:01:00 +0100

    ci: Move iPXE script into file
    To ease debugging
    
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- ci/build.sh
+++ ci/build.sh
@@ -32,24 +32,14 @@ sed -e "s~@NODEBASEURL@~$NODEBASEURL~g" \
 git submodule update --init --recursive --force
 
 [[ -n $BOOBASEURL ]]
+sed -e "s~@BOOBASEURL@~$BOOBASEURL~g" \
+  script0.ipxe.in > ipxe/src/script0.ipxe
+
 pushd ipxe/src
-cat <<EOF > script0.ipxe
-#!ipxe
-set 209:string pxelinux.cfg
-set 210:string $BOOBASEURL
-dhcp || goto manualnet
-chain $BOOBASEURL/pxelinux.0
-:manualnet
-echo Please provide, IP address, Netmask, Gateway and Router
-ifopen net0
-config net0
-chain $BOOBASEURL/pxelinux.0
-EOF
-
 make -j4 EMBED=script0.ipxe
-
 popd
 
+rm -f ovirt-ipxe.iso ovirt-ipxe.usb
 ln -v ipxe/src/bin/ipxe.iso ovirt-ipxe.iso
 ln -v ipxe/src/bin/ipxe.usb ovirt-ipxe.usb
 

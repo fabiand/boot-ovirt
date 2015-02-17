bead10a466af7387411b8fb42d4bb6ca357e2e0f
Fabian Deutsch <fabiand@fedoraproject.org>
2015-02-17 11:36:08 +0100

    Initial commit
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

---
+++ ci/build.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+
+set -ex
+
+#
+# Prepare syslinux
+# https://git.fedorahosted.org/cgit/fedora-infrastructure.git/
+#
+
+sudo yum install -y syslinux
+
+cp -v \
+  /usr/share/syslinux/pxelinux.0 \
+  /usr/share/syslinux/vesa*.c32 \
+  .
+
+
+#
+# Create pxelinux.cfg
+#
+:> pxelinux.cfg
+sed -e "s~@NODEBASEURL@~$NODEBASEURL~" \
+    -e "s~@ENGINEBASEURL@~FIXME~" \
+    pxelinux.cfg.in > pxelinux.cfg
+
+
+#
+# Create iPXE
+#
+pushd ipxe/src
+cat <<EOF > script0.ipxe
+#!ipxe
+set 209:string pxelinux.cfg
+set 210:string $BOOBASE
+dhcp || goto manualnet
+chain $BOOBASE/pxelinux.0
+:manualnet
+echo Please provide, IP address, Netmask, Gateway and Router
+ifopen net0
+config net0
+chain $BOOBASE/pxelinux.0
+EOF
+
+make -j4 EMBED=script0.ipxe
+
+popd
+
+ln -v ipxe/src/bin/ipxe.iso ovirt-ipxe.iso
+ln -v ipxe/src/bin/ipxe.usb ovirt-ipxe.usb
+
